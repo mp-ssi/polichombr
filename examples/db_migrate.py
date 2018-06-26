a283d93635e857451d3966802298c9e4430b27ef
mp-ssi <mp-ssi@users.noreply.github.com>
2018-06-26 11:24:37 +0200

    Fixed polichombr.config imports

--- examples/db_migrate.py
+++ examples/db_migrate.py
@@ -2,8 +2,8 @@
 import imp
 from migrate.versioning import api
 from polichombr import db
-from config import SQLALCHEMY_DATABASE_URI
-from config import SQLALCHEMY_MIGRATE_REPO
+from polichombr.config import SQLALCHEMY_DATABASE_URI
+from polichombr.config import SQLALCHEMY_MIGRATE_REPO
 v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 migration = SQLALCHEMY_MIGRATE_REPO + ('/versions/%03d_migration.py' % (v+1))
 tmp_module = imp.new_module('old_model')
